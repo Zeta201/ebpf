8a9ffdebdacacd33089ad99df60a7ccf3797e1e1
Zeta201 <dini2040gh@gmail.com>
2023-09-18 10:51:09 +0530

    build prog

--- ring_buffers/ring.c
+++ ring_buffers/ring.c
@@ -1,25 +1,44 @@
 #include <uapi/linux/bpf.h>
 
-BPF_PERF_OUTPUT(output); 
- 
-struct data_t {     
+// BCC defines the macro BPF_PERF_OUTPUT for creating a map that will be used to
+// pass messages from the kernel to user space.I’ve called this map output.
+BPF_PERF_OUTPUT(output);
+
+// Every time hello() is run, the code will write a structure’s worth of data. This is
+// the definition of that structure, which has fields for the process ID, the name of
+// the currently running command, and a text message.
+struct data_t
+{
    int pid;
    int uid;
    char command[16];
    char message[12];
 };
- 
-int hello(void *ctx) {
-   struct data_t data = {}; 
+
+int hello(void *ctx)
+{
+   // data is a local variable that holds the data structure to be submitted, and message
+   // holds the "Hello World" string.
+   struct data_t data = {};
    char message[12] = "Hello World";
- 
+
+   // bpf_get_current_pid_tgid() is a helper function that gets the ID of the process
+   // that triggered this eBPF program to run.It returns a 64 - bit value with the process
+   // ID in the top 32 bits.
    data.pid = bpf_get_current_pid_tgid() >> 32;
+   // bpf_get_current_uid_gid() is the helper function you saw in the previous
+   // example for obtaining the user ID.
    data.uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
-   
+   // Similarly, bpf_get_current_comm() is a helper function for getting the name of
+   // the executable(or “command”) that’s running in the process that made the exe
+   //  cve syscall.
    bpf_get_current_comm(&data.command, sizeof(data.command));
-   bpf_probe_read_kernel(&data.message, sizeof(data.message), message); 
- 
-   output.perf_submit(ctx, &data, sizeof(data)); 
- 
+   // bpf_probe_read_kernel() copies it into the right place in the data structure.
+   bpf_probe_read_kernel(&data.message, sizeof(data.message), message);
+   // At this point the data structure is populated with the process ID, command
+   // name, and message.This call to output.perf_submit() puts that data into the
+   //   map.
+   output.perf_submit(ctx, &data, sizeof(data));
+
    return 0;
 }
--- ring_buffers/ring.py
+++ ring_buffers/ring.py
@@ -1,14 +1,46 @@
 #!/usr/bin/python3  
+
+"""
+   Let you write data in a
+   structure of your choosing into a perf ring buffer map.
+
+   A ring buffer as a piece of memory
+logically organized in a ring, with separate “write” and “read” pointers
+
+   If the read pointer catches up with the write pointer, it simply means there’s no data to
+read. If a write operation would make the write pointer overtake the read pointer, the
+data doesn’t get written and a drop counter gets incremented. Read operations include
+the drop counter to indicate whether data has been lost since the last successful read.
+"""
+
+
+
+
+
+
+
+
 from bcc import BPF
 
 b = BPF(src_file="ring.c") 
 syscall = b.get_syscall_fnname("execve")
 b.attach_kprobe(event=syscall, fn_name="hello")
- 
+#print_event is a callback function that will output a line of data to the screen.
+# BCC does some heavy lifting so that I can refer to the map simply as b["out
+# put"] and grab data from it using b["output"].event().  
+
 def print_event(cpu, data, size):  
    data = b["output"].event(data)
    print(f"{data.pid} {data.uid} {data.command.decode()} {data.message.decode()}")
  
+ #b["output"].open_perf_buffer() opens the perf ring buffer. The function
+# takes print_event as an argument to define that this is the callback function to
+# be used whenever there is data to read from the buffer.
 b["output"].open_perf_buffer(print_event) 
+
+# The program will now loop indefinitely,7
+#  polling the perf ring buffer. If there is
+# any data available, print_event will get called
+
 while True:   
    b.perf_buffer_poll()

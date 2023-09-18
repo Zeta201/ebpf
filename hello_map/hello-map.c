017f2cc441a9b590141368e4b70422b0fa152b2a
Zeta201 <dini2040gh@gmail.com>
2023-09-18 10:41:11 +0530

    build prog

--- hello_map/hello-map.c
+++ hello_map/hello-map.c
@@ -1,19 +1,35 @@
 
 #include <uapi/linux/bpf.h>
 
+// BPF_HASH() is a BCC macro that defines a hash table map
 BPF_HASH(counter_table);
 
-int hello(void *ctx) {
+int hello(void *ctx)
+{
    u64 uid;
    u64 counter = 0;
    u64 *p;
 
+   // bpf_get_current_uid_gid() is a helper function used to obtain the user ID that
+   // is running the process that triggered this kprobe event.The user ID is held in the
+   // lowest 32 bits of the 64 -bit value that gets returned
    uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
+   // Look for an entry in the hash table with a key matching the user ID. It returns a
+   // pointer to the corresponding value in the hash table.
    p = counter_table.lookup(&uid);
-   if (p != 0) {
+   /*
+    If there is an entry for this user ID, set the counter variable to
+    the current value in the hash table (pointed to by p). If there is no entry
+    for this user ID in the hash table, the pointer will be 0, and the counter value will be left at 0.
+   */
+   if (p != 0)
+   {
       counter = *p;
    }
+   // Whatever the current
+   // counter value is, it gets incremented by one.
    counter++;
+   // Update the hash table with the new counter value for this user ID.
    counter_table.update(&uid, &counter);
    return 0;
 }
--- hello_map/hello-map.py
+++ hello_map/hello-map.py
@@ -1,19 +1,50 @@
 #!/usr/bin/python3  
 from bcc import BPF
 from time import sleep
+"""
+    A map is a data structure that can be accessed from an eBPF program and from user
+space.
+    Usecases
+    1. User space writing configuration information to be retrieved by an eBPF
+program
+    2. An eBPF program storing state, for later retrieval by another eBPF program (or a
+    future run of the same program)
+    3. An eBPF program writing results or metrics into a map, for retrieval by the user
+    space app that will present results
 
+    Types of maps
+    1. Array maps
+    2. Maps optimized for FIFO LIFO 
+    3. sockmaps and devmaps hold information about sockets and network
+    devices used by eBPF programs
+    4. Hash maps
+    5. Perf and ring buffers
 
-
+"""
+"""
+ Hash Table Map Demo
+ Key Value pairs where key is the user id and value is the number of times
+ execve is called by a process under that user id
+"""
+"""
+The program is compiled, loaded into the kernel,
+ and attached to the execve kprobe.
+"""
 b = BPF(src_file="hello-map.c")
+
 syscall = b.get_syscall_fnname("execve")
 b.attach_kprobe(event=syscall, fn_name="hello")
 
 # Attach to a tracepoint that gets hit for all syscalls 
 # b.attach_raw_tracepoint(tp="sys_enter", fn_name="hello")
 
+# This part of the code loops indefinitely, looking for output to display every two
+# seconds
 while True:
     sleep(2)
     s = ""
+    # BCC automatically creates a Python object to represent the hash table. This code
+    # loops through any values and prints them to the screen.
     for k,v in b["counter_table"].items():
         s += f"ID {k.value}: {v.value}\t"
     print(s)
